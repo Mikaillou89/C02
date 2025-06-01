7d2721581fbd8298ffe0d13aafef9782605e9619
Mikaillou89 <mikaillou25@gmail.com>
2025-06-01 16:58:57 +0100

    Update hello_world_c.c

--- hello_world_c.c
+++ hello_world_c.c
@@ -1,6 +1,10 @@
 #include <stdio.h> 
-
+//Inclusion de la bibliothèque standard d’entrée/sortie (pour utiliser printf). 
 int main() { 
-printf("Hello, World!\n"); 
+//Déclaration de la fonction principale main, point d’entrée du programme. 
+  printf("Hello, World!\n");
+  //Affiche le message "Hello, World!" suivi d’un saut de ligne (\n) à l’écran. 
 return 0; 
+//Termine la fonction principale et retourne 0, indiquant une exécution réussie.
 }
+//Fin de la fonction main. 

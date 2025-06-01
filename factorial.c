d9cf249ac6cfb19e6e5c5a2df4063bf7e4dbfc4f
Mikaillou89 <mikaillou25@gmail.com>
2025-06-01 17:48:03 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -3,16 +3,23 @@
 int main() {
 int nombre = 0;
 int i = 1;
+// nombre contiendra l’entrée utilisateur ; i est un compteur pour la boucle.
 scanf("%d", &nombre);
+//  Lecture de l'entier nombre. 
 if (nombre < 0) {
 printf ("Erreur.\n");
-} else {
+} 
+// Vérifie si l'utilisateur a entré un nombre négatif. Si oui, affiche une erreur. 
+else {
 int fact = 1; 
+// Initialise fact à 1 pour calculer la factorielle. 
 while (i <= nombre) {
 fact *= i;
 i++;
 }
+// Boucle qui multiplie fact par i de 1 à nombre. 
 printf("Le factoriel de %d est : %d\n", nombre, fact);
 }
 return 0;
 }
+// Affiche le résultat et termine le programme. 

ffa5929cdcc4331a81bd0263ba3cfed6bb8a7270
Mikaillou89 <mikaillou25@gmail.com>
2025-06-01 17:56:49 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,11 +2,17 @@
 
 int main() {
 int a, b;
+// Déclaration de deux variables entières. 
 scanf("%d %d", &a, &b);
-
+//  Lecture de deux entiers. 
 a = a + b;
 b = a - b;
 a = a - b;
+//  Échange des valeurs de a et b sans utiliser de variable temporaire : 
+//• a devient a + b 
+//• b devient (a + b) - b = a 
+//• a devient (a + b) - a = b
 printf("Après échange : a = %d, b = %d\n", a, b);
 return 0;
 }
+// Affiche les nouvelles valeurs après l’échange et termine le programme.
